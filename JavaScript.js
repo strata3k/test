8c4b17e1056cea639f19f987655a930f92ae8ece
strata3k <benyang217@yahoo.com>
2022-04-29 10:11:18 -0700

    45

--- JavaScript.js
+++ JavaScript.js
@@ -70,71 +70,163 @@ const myStory = {
 		]
 	},
 	Bob: {
-		mainText: `<h2>Bob is the beast who sits upon the bridge of Yarn. </h2>`,
+		mainText: `<h2>"Bob is the beast who sits upon the bridge of Yarn." </h2>`,
 		option: [
-			['Wake up','woken'],
+			['How get the brain?','weapons'],
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	weapons: {
+		mainText: `<h2>"I see you really want to escape from here. Here let me get something from the chest." </h2>`,
 		option: [
-			['Wake up','woken'],
+			['wait','weapons2'],
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	weapons2: {
+		mainText: `<h2>"Ah, thankfully I found some leftover gear. Be very careful when you go to into the city, danger lurks every corner adventuerer." You receive a blunderbust and a serrated sword . You exit the chapal and notice the gravestone now has frogs on it and a lit lamp. </h2>`,
 		option: [
-			['Wake up','woken'],
+			['Go to gravestone','gravestoneOpen'],
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	gravestone: {
+		mainText: `<h2>It's just a gravestone </h2>`,
 		option: [
-			['Wake up','woken'],
+			['Go to chapal','chapal'],
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	gravestoneOpen: {
+		mainText: `<h2>You walk to the gravestone and touch the lamp. As you touch the lamp you fade away again. </h2>`,
 		option: [
-			['Wake up','woken'],
+			['Back to Yarn','intoYarn'],
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	intoYarn: {
+		mainText: `<h2>As you come back to the city of Yarn you form at the lamp post in the clinic. </h2>`,
 		option: [
-			['Wake up','woken'],
+			['Walk out','outside'],
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	outside: {
+		mainText: `<h2>Walking through the empty halls of the clinic and out the door you go. You are now entering the city of Yarn. You see this light coming from a distance. You hear the sound of walking on this stone path. 3 humanoid figures emerge from the stone path and they notice you. You can see them muttering something with their torches and weapons in their hands." </h2>`,
 		option: [
-			['Wake up','woken'],
+			['Walk to them','walk'],
+			['Stay back','stay']
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	walk: {
+		mainText: `<h2>"You there, you contaminated this city with the Bad Blood didn't you! You plague ridden rat!" They charge at you </h2>`,
 		option: [
-			['Wake up','woken'],
+			['Shoot with their blunderbust','blunderbust'],
+			['Hit them with your sword', 'sword'],
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	blunderbust: {
+		mainText: `<h2>You shoot at the 3 and they all fall down. Dead. The body then starts to fade away into dust. </h2>`,
 		option: [
-			['Wake up','woken'],
+			['','woken'],
+			['']
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	sword: {
+		mainText: `<h2>When two of the three humanoids come close to you, you swing your blade. Cutting down both of them in one swoop. The smell of rotting flesh captures your nose and dissappears as they fade into dust. The final enemy is coming at you with their cleaver in hand coming to swing. </h2>`,
 		option: [
-			['Wake up','woken'],
+			['Evade','evade'],
+			['Ripose', 'riposte']
 		]
 	},
-	Accept: {
-		mainText: `<h2>You accept the treatment and signed a contract. You are now put into a coma because of the anesthetic. </h2>`,
+	evade: {
+		mainText: `<h2>You roll out of the cleaver's swipe </h2>`,
 		option: [
-			['Wake up','woken'],
+			['slash with sword','sword2'],
 		]
 	},
+	sword2 : {
+		mainText: `<h2>You put to rest the final enemy. They drop a small vile of blood. </h2>`,
+		option: [
+			['Take vile', 'vileObtain'],
+		]
+	},
+	
+	riposte: {
+		mainText: `<h2>You try to riposte the attack. But, with no real training you are killed by the butcher. </h2>`,
+		option: [
+			['Respawn','respawn1'],
+		]
+	},	
+	
+	respawn1: {
+		mainText: `<h2>You feel rejuvinated after your dying. It was like nothing had happened.  </h2>`,
+		option: [
+			['walk out the clinic','clinic2'],
+		]
+	},	
+	clinic2: {
+		mainText: `<h2>You walk out the clinic seeing the humanoid that killed you </h2>`,
+		option: [
+			['charge at them','lastOne'],
+		]
+	},	
+	lastOne: {
+		mainText: `<h2>You charged at the final humanoid, killing it. It drops this vile full of blood. </h2>`,
+		option: [
+			['Take vile','vileObtain'],
+		]
+	},	
+	vileObtain: {
+		mainText: `<h2>You obtained the blood </h2>`,
+		option: [
+			['Drink the blood','drink'],
+			['Walk to the bridge', 'bridge'],
+		]
+	},	
+	drink: {
+		mainText: `<h2>You drank the blood from the small vile. You feel rejuvinated. </h2>`,
+		option: [
+			['walk to bridge','bridge'],
+		]
+	},	
+	bridge: {
+		mainText: `<h2> </h2>`,
+		option: [
+			['slash with sword','sword2'],
+		]
+	},	
+	bridge: {
+		mainText: `<h2>You roll out of the cleaver's swip </h2>`,
+		option: [
+			['slash with sword','sword2'],
+		]
+	},	
+	evade: {
+		mainText: `<h2>You roll out of the cleaver's swipe </h2>`,
+		option: [
+			['slash with sword','sword2'],
+		]
+	},	
+	evade: {
+		mainText: `<h2>You roll out of the cleaver's swipe </h2>`,
+		option: [
+			['slash with sword','sword2'],
+		]
+	},	
+	evade: {
+		mainText: `<h2>You roll out of the cleaver's swipe </h2>`,
+		option: [
+			['slash with sword','sword2'],
+		]
+	},	
+	evade: {
+		mainText: `<h2>You roll out of the cleaver's swipe </h2>`,
+		option: [
+			['slash with sword','sword2'],
+		]
+	},	
+	evade: {
+		mainText: `<h2>You roll out of the cleaver's swipe </h2>`,
+		option: [
+			['slash with sword','sword2'],
+		]
+	},	
+
 
 
 
